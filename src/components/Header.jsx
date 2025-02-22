588c1277e160bbce9bcd87502a8dd83d8baf5d90
Siddharth <117755306+siddharth-1108@users.noreply.github.com>
2025-02-22 21:57:27 +0530

    Make product page responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,10 @@ import { IoMenu } from 'react-icons/io5';
 const Header = () => {
 	const [showMenu, setShowMenu] = useState(false)
 
+	const toggleDropdown = () => {
+		setShowMenu((prev) => !prev);
+	};
+
 	return (<header>
 		<div className='logo'>
 			<NavLink to='/'>
@@ -22,14 +26,14 @@ const Header = () => {
 				<li><NavLink to='/contact-us' className='contact-us' >Contact Us</NavLink></li>
 			</ul>
 		</nav>
-		<div className='menu-icon' onClick={() => setShowMenu(!showMenu)}>
+		<div className='menu-icon' onClick={toggleDropdown}>
 			<IoMenu />
 		</div>
 		<ul className='dropdown-menu' style={showMenu ? { display: 'block' } : { display: 'none' }}>
-			<li><NavLink to='/' className={({ isActive }) => isActive ? 'active-link' : ''} >Home</NavLink></li>
-			<li><NavLink to='/products' className={({ isActive }) => isActive ? 'active-link' : ''} >Products</NavLink></li>
-			<li><NavLink to='/about' className={({ isActive }) => isActive ? 'active-link' : ''} >About</NavLink></li>
-			<li><NavLink to='/contact-us' className={({ isActive }) => isActive ? 'active-link' : ''} >Contact Us</NavLink></li>
+			<li><NavLink to='/' className={({ isActive }) => isActive ? 'active-link' : ''} onClick={toggleDropdown} >Home</NavLink></li>
+			<li><NavLink to='/products' className={({ isActive }) => isActive ? 'active-link' : ''} onClick={toggleDropdown} >Products</NavLink></li>
+			<li><NavLink to='/about' className={({ isActive }) => isActive ? 'active-link' : ''} onClick={toggleDropdown} >About</NavLink></li>
+			<li><NavLink to='/contact-us' className={({ isActive }) => isActive ? 'active-link' : ''} onClick={toggleDropdown} >Contact Us</NavLink></li>
 		</ul>
 	</header>)
 }
